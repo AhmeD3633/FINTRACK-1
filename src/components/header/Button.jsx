9fb6a6fe18e8e112fc0b19856faca32034f7c6cc
AhmeD3633 <143659754+AhmeD3633@users.noreply.github.com>
2024-11-14 22:42:36 +0200

    spaces-update

--- src/components/header/Button.jsx
+++ src/components/header/Button.jsx
@@ -5,7 +5,7 @@ const Button = () => {
     <div className="flex justify-center">
       {" "}
       {/* Center the button within the div */}
-      <button className="btn btn-outline sm:w-[188px] h-[52px] rounded-full text-[#8B6FC4] lg:text-[#5070D1]  font-semibold text-[18px] leading-4 hover:text-white 2xl:hover:bg-[#5070D1] hover:bg-[#8B6FC4] w-40">
+      <button className="btn btn-outline sm:w-[188px] h-[52px] rounded-full text-[#5070D1] :text-[#5070D1]  font-semibold text-[18px] leading-4 hover:text-white 2xl:hover:bg-[#5070D1] hover:bg-[#5070D1] w-40">
         Hire Me
       </button>
     </div>
