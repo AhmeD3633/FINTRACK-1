65141d7a958e07623f0be50d9647d2e81c292ae2
AhmeD3633 <143659754+AhmeD3633@users.noreply.github.com>
2024-11-25 22:43:56 +0200

    Resume-modal-changes

--- src/components/header/ResumeModal.jsx
+++ src/components/header/ResumeModal.jsx
@@ -6,12 +6,12 @@ const ResumeModal = () => {
       {/* Daisy UI Modal */}
       <dialog id="my_modal_2" className="modal">
         <div className="modal-box bg-slate-50">
-          <h3 className="font-bold text-lg">Download My Resume</h3>
+          <h3 className="font-bold text-xl">Download My Resume</h3>
           <p className="py-4">Click the button below to download my CV.</p>
           <a
-            // href="public/Omar Bendary's CV.pdf"
+            href="public/Omar Bendary's CV.pdf"
             download
-            className="btn mt-4 bg-[#8B6FC4] text-white hover:bg-[#7257A0] hover:text-white"
+            className="btn mt-4 bg-[#8B6FC4] text-white hover:bg-[#7257A0] hover:text-white rounded-full px-8"
           >
             Download CV
           </a>
